8ea5ae0cb0b207fd2da57ec60cac83ee88cab935
kirafaye99 <kirademarco99@gmail.com>
2021-08-23 17:24:01 -0700

    still trying to figure out how to save

--- routes/view.js
+++ routes/view.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const router = require('express').Router();
+// const path = require('path');
+// const router = require('express').Router();
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
-
-// router.get('/api/notes', (req, res) => {
-//     res.json(db);
-// })
-// router.post('api/notes', (req, res) => {
-//     const noteElement = createNote(req.body, db);
+// router.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/notes.html'))
 // })
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+// // router.get('/api/notes', (req, res) => {
+// //     res.json(db);
+// // })
+// // router.post('api/notes', (req, res) => {
+// //     const noteElement = createNote(req.body, db);
+// // })
 
-module.exports = router;
+// router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 const express = require('express');
-// const apiRoute = require('./routes/api');
-// const viewRoute = require('./routes/view');
-const uuid = require("uuid").v4;
 const fs = require('fs');
+const uuid = require("uuid").v4;
+const db = require('./db/db.json');
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.use('/api', apiRoute);
-// app.use('/', viewRoute);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+app.post('/api/notes', (req, res) => {
+
+    const noteEl = genNote(req.body, db);
+    res.json(noteEl);
+})
+
+function genNote(body, event) {
+    const noteElement = {
+        id: uuid(),
+        title: body.title,
+        text: body.text,
+    };
+
+    let noteArray = event || [];
+    noteArray.push(noteElement);
+
+    fs.writeFileSync( path.join(__dirname, db), JSON.stringify(noteArray));
+    return noteElement;
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
